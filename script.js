68fa9b3787958e2304b54dea7625e7a2b94d3e9f
Shreyas M <137000011+ShreyasM-codes@users.noreply.github.com>
2024-02-15 14:00:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const inputBox = document.getElementById('input-box');
+const listContainer = document.getElementById('list-container');
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something")
+    }else{
+        let li=document.createElement('li');
+        li.innerHTML=inputBox.value;
+        listContainer.appendChild(li);  //li should be displayed inside list container
+        let span = document.createElement('span');  // after writing text X should be displayed for cancelling it
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
+    }
+
+    inputBox.value = '';
+    saveData();
+}
+document.getElementById('add-button').addEventListener('click',addTask);
+
+listContainer.addEventListener("click",function(e){
+    if(e.target.tagName === 'LI'){
+        e.target.classList.toggle('checked');
+        saveData();
+    }else if(e.target.tagName === 'SPAN'){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+function saveData(){
+    localStorage.setItem('data',listContainer.innerHTML);
+}
+
+function viewTask(){
+    listContainer.innerHTML = localStorage.getItem('data');
+}
+viewTask();
